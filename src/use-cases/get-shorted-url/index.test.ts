fad418724f64384ff1e79403af03fe6fa71c7b29
joaomarcosc <joaomarcos.developer.personal@gmail.com>
2024-11-12 02:26:30 -0300

    feat: get shorted url use case with test

---
+++ src/use-cases/get-shorted-url/index.test.ts
@@ -0,0 +1,41 @@
+import { UrlRepositoryInMemory } from "@/repositories/in-memory/url-repository-in-memory";
+import type { Selectable } from "kysely";
+import type { Url } from "kysely-codegen";
+import { beforeEach, describe, expect, it } from "vitest";
+import { GetShortedUrlUseCase } from ".";
+import { ResourceNotFoundError } from "../errors/resource-not-found";
+import { ShortenerUrlUseCase } from "../shortener-url";
+
+describe("Get shorted url use case", () => {
+  let urlRepository: UrlRepositoryInMemory;
+  let shortenerUrlUseCase: ShortenerUrlUseCase;
+  let sut: GetShortedUrlUseCase;
+  let url: Selectable<Url> | undefined;
+
+  const origUrl = "https://www.google.com/";
+
+  beforeEach(async () => {
+    urlRepository = new UrlRepositoryInMemory();
+    shortenerUrlUseCase = new ShortenerUrlUseCase(urlRepository);
+    sut = new GetShortedUrlUseCase(urlRepository);
+
+    url = await shortenerUrlUseCase.execute({
+      origUrl,
+    });
+  });
+
+  it("should be can GET a shorted url", async () => {
+    const shortedUrl = await sut.execute({
+      urlId: url?.urlId ?? "",
+    });
+    expect(shortedUrl.origUrl).toEqual(origUrl);
+  });
+
+  it("should be NOT get a shorted url with wrong urlId", async () => {
+    expect(
+      sut.execute({
+        urlId: "WRONG URL ID",
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
---
+++ src/use-cases/get-shorted-url/index.ts
@@ -0,0 +1,22 @@
+import type { UrlRepository } from "@/repositories/url-repository";
+import { ResourceNotFoundError } from "../errors/resource-not-found";
+
+interface GetShortedUrlUseCaseParams {
+  urlId: string;
+}
+
+export class GetShortedUrlUseCase {
+  constructor(private urlRepository: UrlRepository) {}
+
+  async execute({ urlId }: GetShortedUrlUseCaseParams) {
+    const url = await this.urlRepository.findOne({ urlId });
+
+    if (!url) {
+      throw new ResourceNotFoundError("url");
+    }
+
+    await this.urlRepository.updateOne(url.urlId, url.clicks + 1);
+
+    return url;
+  }
+}
